9defdcd1f3f1a38de093b3fc05f2e90f17c8f471
Aditya <choudharyaditya9381@gmail.com>
2025-04-28 23:18:38 +0530

    // 1 or true similar count or true is first run // 0 or false similar count

---
+++ arrya_interview.php
@@ -0,0 +1,9 @@
+<?php
+
+// 1 or true similar count or true is first run 
+// 0 or false similar count 
+
+$array = array(20=>"apple",1=>"a","1"=>"b",true =>"d",0=>"e");
+
+echo $array[0]."<br>"; // e
+?>
