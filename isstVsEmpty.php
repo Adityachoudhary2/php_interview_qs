1b9ae3698c551bbe8419dadfa8564e7e4f31a581
Aditya <choudharyaditya9381@gmail.com>
2025-04-29 22:36:02 +0530

    isset and empty method

---
+++ isstVsEmpty.php
@@ -0,0 +1,24 @@
+ <?php
+ $diff = NULL;
+
+ $var1 = 0;
+    $var2 = 0.0;
+    $var3 = "0";
+    $var4 = false;
+    $var5 = array();
+    $var6 = "";
+
+// isset() vs empty()
+if(isset($diff)){
+    echo "if";
+
+}else{
+    echo "else";
+}
+if(empty($diff)){
+    echo "if";
+}else{
+    echo "else";
+}
+ 
+ ?>
