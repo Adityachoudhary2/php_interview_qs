cb1d40807b8fde1966d282a10d72767a204d4b83
Aditya <choudharyaditya9381@gmail.com>
2025-04-29 14:32:52 +0530

    nested function call

---
+++ NestedFunction.php
@@ -0,0 +1,15 @@
+ <?php
+ 
+ function apple(){
+    function banana(){
+        return "banana";
+    }
+    return "apple";
+
+ }
+ 
+
+ echo apple(); // Output: apple
+    echo "<br>";
+ echo banana();
+ ?>
