375a0d8d5649dd809ccb7bc3bbfa05a914b197b9
Aditya <choudharyaditya9381@gmail.com>
2025-04-29 14:25:57 +0530

    Array logical

---
+++ ArrayLogical.php
@@ -0,0 +1,15 @@
+ <?php
+ $num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//  echo count($num) . "\n"; // 10 predefine array length//
+
+// user define 
+$count = 0;
+foreach($num as $item){
+    $count++;
+
+}
+echo $count . "\n"; // 10 predefine array length//
+ 
+ 
+ ?>
